7906d5d44b8e2a04b7b280e8515675393014fb8f
Lars de Klijne <larsdeklijne@gmail.com>
2020-05-20 12:53:10 +0100

    created migrations for logopedisten, locaties_logopedisten,  locaties. deleted old migrations

---
+++ database/migrations/2020_05_13_082541_create_logopedisten.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLogopedisten extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('logopedisten', function (Blueprint $table) {
+            $table->increments('id')->unsigned();
+            $table->string('voornaam');
+            $table->string('tussenvoegsel');
+            $table->string('achternaam');
+            $table->string('wachtwoord');
+            $table->string('email')->unique();
+
+            $table->timestamps();
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('logopedisten');
+    }
+}
