fee5a6f0f66d07e1fe177405cb66a13d4bb87fb2
Lars de Klijne <larsdeklijne@gmail.com>
2020-05-21 14:38:25 +0100

    created new seeder to generate testdata //  deleted old seeder

---
+++ database/seeds/TestData.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class TestData extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $logopedistId = rand(1, 100);
+        $patientId = rand(1, 100);
+        $timestamp = mt_rand(1, time());
+
+        DB::table('logopedisten')->insert([
+            'id' => $logopedistId,
+            'voornaam' => Str::random(10),
+            'tussenvoegsel' => 'de',
+            'achternaam' => Str::random(10),
+            'wachtwoord' => Hash::make('wachtwoord'),
+            'locaties' => 'Zoetermeer, leiden',
+            'email' => Str::random(10).'@gmail.com',
+        ]);
+
+        DB::table('patienten')->insert([
+            'id' => $patientId,
+            'patient_nummer' => rand(1000, 5000),
+            'geboortedatum' => '8/8/8',
+            'locaties' => 'Zoetermeer, leiden',
+            'wachtwoord' => Hash::make('wachtwoord'),
+        ]);
+
+        DB::table('logopedisten_patienten')->insert([
+            'id' => rand(1, 100),
+            'logopedist_id' => $logopedistId,
+            'patient_id' => $patientId,
+        ]);
+
+        DB::table('adviezen')->insert([
+            'id' => rand(1, 100),
+            'patient_id' => $patientId,
+            'advies' => Str::random(10),
+            'beknopt_advies' => Hash::make('wachtwoord'),
+            'zichtbaar' => 0,
+        ]);
+
+    }
+}
