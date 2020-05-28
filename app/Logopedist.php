b1030f65c318fa67265442633ab8e5b7884012a3
Lars de Klijne <larsdeklijne@gmail.com>
2020-05-28 19:35:41 +0100

    configured fillable fields logopedist model

--- app/Logopedist.php
+++ app/Logopedist.php
@@ -7,4 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class Logopedist extends Model
 {
     protected $table = 'logopedisten';
+
+    protected $fillable = ['voornaam', 'tussenvoegsel', 'achternaam', 'wachtwoord', 'locaties', 'email'];
 }
