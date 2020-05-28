45d9711570e22f71ba29032401868a98b1cad102
Lars de Klijne <larsdeklijne@gmail.com>
2020-05-28 20:03:55 +0100

    created get/ add functions in adviescontroller

--- app/Http/Controllers/AdviesController.php
+++ app/Http/Controllers/AdviesController.php
@@ -3,8 +3,35 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use DB;
 
 class AdviesController extends Controller
 {
-    //
+
+    public function get($id)
+    {
+        $advies = DB::table('adviezen')
+                    ->where('id', $id)
+                    ->first();
+
+        dd($advies);
+    }
+
+    public function add(Request $request)
+    {
+        // patienten fields
+            // id
+            // patient_nummer
+            // geboortedatum
+            // locaties
+            // wachtwoord
+        
+    }
+
+    /*
+        public function delete($id)
+        {
+            
+        }
+    */
 }
