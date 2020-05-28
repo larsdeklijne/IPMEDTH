52ac1890f4ac569efdad8279e807e8707313e22d
Lars de Klijne <larsdeklijne@gmail.com>
2020-05-28 19:49:11 +0100

    created index/ get function for patient

--- app/Http/Controllers/PatientenController.php
+++ app/Http/Controllers/PatientenController.php
@@ -3,8 +3,40 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use DB;
 
 class PatientenController extends Controller
 {
-    //
+    public function index()
+    {
+        $allePatienten = DB::table('patienten')->get();
+    }
+
+    public function get($id)
+    {
+        $patient = DB::table('patienten')
+                        ->where('id', $id)
+                        ->first();
+
+        dd($patient);
+    }
+
+    public function add(Request $request)
+    {
+        // patienten fields
+            // id
+            // patient_nummer
+            // geboortedatum
+            // locaties
+            // wachtwoord
+        
+    }
+
+    /*
+        public function delete($id)
+        {
+            
+        }
+    */
+
 }
