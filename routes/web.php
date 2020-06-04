b3833f42ee730e87a1843a1a31ee1b3ed478b3ca
Lars de Klijne <larsdeklijne@gmail.com>
2020-06-04 15:20:47 +0100

    added cors middleware

--- routes/web.php
+++ routes/web.php
@@ -2,29 +2,30 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::middleware('cors')->group(function () {
+    // Laravel homepage
+    Route::get('/', function () {
+        return view('welcome');
+    });
 
-// Laravel homepage
-Route::get('/', function () {
-    return view('welcome');
+    Route::get('/test', function () {
+        return view('welcome');
+    });
+
+    // Logopedisten routes
+    Route::get('/logopedist/index', 'LogopedistenController@index');
+    Route::get('/logopedist/get/{id}', 'LogopedistenController@get');
+    Route::post('/logopedist/add', 'LogopedistenController@add');
+
+    // Patienten routes
+    Route::get('/patient/index', 'PatientenController@index');
+    Route::get('/patient/get/{id}', 'PatientenController@get');
+    Route::post('/patient/add', 'PatientenController@add');
+
+    // Advies routes
+    Route::get('/advies/get/{id}', 'AdviesController@get');
+    Route::post('/advies/add', 'AdviesController@add');
 });
-
-Route::get('/test', function () {
-    return view('welcome');
-});
-
-// Logopedisten routes
-Route::get('/logopedist/index', 'LogopedistenController@index');
-Route::get('/logopedist/get/{id}', 'LogopedistenController@get');
-Route::post('/logopedist/add', 'LogopedistenController@add');
-
-// Patienten routes
-Route::get('/patient/index', 'PatientenController@index');
-Route::get('/patient/get/{id}', 'PatientenController@get');
-Route::post('/patient/add', 'PatientenController@add');
-
-// Advies routes
-Route::get('/advies/get/{id}', 'AdviesController@get');
-Route::post('/advies/add', 'AdviesController@add');
 
 
 
