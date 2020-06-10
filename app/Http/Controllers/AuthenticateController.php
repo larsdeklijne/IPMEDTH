169f4c064dfe3cd0a22326f9ac0cb4c41be7ec0c
Lars de Klijne <larsdeklijne@gmail.com>
2020-06-10 09:36:49 +0100

    configured authenticaController, created index function which returns allLogopedisten

--- app/Http/Controllers/AuthenticateController.php
+++ app/Http/Controllers/AuthenticateController.php
@@ -1,17 +1,29 @@
 <?php
+
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Requests;
 use JWTAuth;
 use Tymon\JWTAuthExceptions\JWTException;
+use App\Logopedist;
 
 class AuthenticateController extends Controller
 {
+    
+    public function __construct()
+    {
+        // Apply the jwt.auth middleware to all methods in this controller
+        // except for the authenticate method. We don't want to prevent
+        // the user from retrieving their token if they don't already have it
+        $this->middleware('jwt.auth', ['except' => ['authenticate']]);
+    }
 
     public function index()
     {
-        // TODO: show users
-    }    
+        // Retrieve all the users in the database and return them
+        $logopedisten = Logopedist::all();
+        return $logopedisten;
+    }
 
     public function authenticate(Request $request)
     {
