d58c094e05790d0ea08ed4c72ab729d5ef444e24
Lars de Klijne <larsdeklijne@gmail.com>
2020-06-10 16:24:28 +0100

    changed fillable fields in patienten model

--- app/Patienten.php
+++ app/Patienten.php
@@ -8,5 +8,5 @@ class Patienten extends Model
 {
     protected $table = 'patienten';
 
-    protected $fillable = ['patient_nummer', 'geboortedatum', 'locaties', 'wachtwoord'];
+    protected $fillable = ['patient_nummer', 'geboortedatum', 'locaties', 'wachtwoord', 'gehaste_wachtwoord'];
 }
