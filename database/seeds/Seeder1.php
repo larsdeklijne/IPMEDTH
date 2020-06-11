1acdf9deeb7431e75c551621ba9050c94c732a67
Lars de Klijne <larsdeklijne@gmail.com>
2020-06-11 19:31:24 +0100

    created 2 new seeders

---
+++ database/seeds/Seeder1.php
@@ -0,0 +1,73 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class Seeder1 extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // logopedist
+        DB::table('logopedisten')->insert([
+            'id' => 1,
+            'voornaam' => 'Yvon',
+            'tussenvoegsel' => 'van',
+            'achternaam' => 'Dijk',
+            'password' => Hash::make('password'),
+            'locaties' => 'Leiden',
+            'email' => 'yvonvandijk@gmail.com',
+        ]);
+        
+        // patient 1
+        DB::table('patienten')->insert([
+            'id' => 1,
+            'patient_nummer' => rand(1000, 5000),
+            'geboortedatum' => '1995/9/8',
+            'locaties' => 'Leiden',
+            'wachtwoord' => Hash::make('password'),
+        ]);
+
+        // link tussen logopedist en patient 1
+        DB::table('logopedisten_patienten')->insert([
+            'logopedist_id' => 1,
+            'patient_id' => 1,
+        ]);
+        
+        // patient 2
+        DB::table('patienten')->insert([
+            'id' => 2,
+            'patient_nummer' => rand(1000, 5000),
+            'geboortedatum' => '1990/10/2',
+            'locaties' => 'Leiden',
+            'wachtwoord' => Hash::make('password'),
+        ]);
+
+        // link tussen logopedist en patient 2
+        DB::table('logopedisten_patienten')->insert([
+            'logopedist_id' => 1,
+            'patient_id' => 2,
+        ]);
+
+        // patient 3
+        DB::table('patienten')->insert([
+            'id' => 3,
+            'patient_nummer' => rand(1000, 5000),
+            'geboortedatum' => '1985/5/4',
+            'locaties' => 'Leiden',
+            'wachtwoord' => Hash::make('password'),
+        ]);
+
+        // link tussen logopedist en patient 2
+        DB::table('logopedisten_patienten')->insert([
+            'logopedist_id' => 1,
+            'patient_id' => 3,
+        ]);
+
+    
+
+    }
+}
---
+++ database/seeds/Seeder2.php
@@ -0,0 +1,70 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class Seeder2 extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // logopedist
+        DB::table('logopedisten')->insert([
+            'id' => 2,
+            'voornaam' => 'Jan',
+            'tussenvoegsel' => 'de',
+            'achternaam' => 'Jong',
+            'password' => Hash::make('password'),
+            'locaties' => 'Den-haag',
+            'email' => 'jandejong@gmail.com',
+        ]);
+        
+        // patient 1
+        DB::table('patienten')->insert([
+            'id' => 4,
+            'patient_nummer' => rand(1000, 5000),
+            'geboortedatum' => '1997/5/8',
+            'locaties' => 'Den-haag',
+            'wachtwoord' => Hash::make('password'),
+        ]);
+
+        // link tussen logopedist en patient 1
+        DB::table('logopedisten_patienten')->insert([
+            'logopedist_id' => 2,
+            'patient_id' => 4,
+        ]);
+        
+        // patient 2
+        DB::table('patienten')->insert([
+            'id' => 5,
+            'patient_nummer' => rand(1000, 5000),
+            'geboortedatum' => '1992/5/8',
+            'locaties' => 'Den-haag',
+            'wachtwoord' => Hash::make('password'),
+        ]);
+
+        // link tussen logopedist en patient 1
+        DB::table('logopedisten_patienten')->insert([
+            'logopedist_id' => 2,
+            'patient_id' => 5,
+        ]);
+
+       // patient 1
+       DB::table('patienten')->insert([
+            'id' => 6,
+            'patient_nummer' => rand(1000, 5000),
+            'geboortedatum' => '200/2/8',
+            'locaties' => 'Den-haag',
+            'wachtwoord' => Hash::make('password'),
+        ]);
+
+        // link tussen logopedist en patient 1
+        DB::table('logopedisten_patienten')->insert([
+            'logopedist_id' => 2,
+            'patient_id' => 6,
+        ]);
+    }
+}
