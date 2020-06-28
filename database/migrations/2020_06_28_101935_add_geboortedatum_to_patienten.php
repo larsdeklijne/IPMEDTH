7e42b37ad9bcf42012a2a34cf82ad350cec5b5bd
Lars de Klijne <larsdeklijne@gmail.com>
2020-06-28 11:20:27 +0100

    new migration

---
+++ database/migrations/2020_06_28_101935_add_geboortedatum_to_patienten.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddGeboortedatumToPatienten extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('patienten', function (Blueprint $table) {
+            $table->date('geboortedatum');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        
+    }
+}
